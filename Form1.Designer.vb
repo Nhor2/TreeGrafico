215f1b00859d36d3cc0d3249ab4eddbecd99c08e
Nhor2 <117581117+Nhor2@users.noreply.github.com>
2024-03-28 11:53:54 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(850, 500)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Seleziona la cartella"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(26, 114)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(245, 35)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Sfoglia..."
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.Location = New System.Drawing.Point(26, 155)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(386, 191)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel1.Location = New System.Drawing.Point(533, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(863, 521)
+        Me.Panel1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 373)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(368, 16)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Seleziona il Font Titolo e Tree Grafico             Colore"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(26, 392)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 37)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Titolo..."
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(166, 392)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(134, 37)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Tree..."
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.White
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(330, 392)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(134, 37)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Colore..."
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(11, 450)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(208, 16)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Seleziona il Colore di Sfondo"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(26, 469)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(134, 37)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Sfondo..."
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Blue
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(470, 392)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(23, 35)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = " "
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(166, 469)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 35)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = " "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(327, 450)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 16)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Salva..."
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(330, 469)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(134, 37)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Salva..."
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.TreeGrafico.My.Resources.Resources.TreeGrafico_icona
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 92)
+        Me.PictureBox2.TabIndex = 16
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1396, 521)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1412, 560)
+        Me.MinimumSize = New System.Drawing.Size(1412, 560)
+        Me.Name = "Form1"
+        Me.Text = "TreeGrafico"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button6 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+
+    'TreeGrafico
+    'lord.marte@gmail.com
+    '28-03-2024
+    'creazione di un albero grafico
+
+
+    Dim Tree_path As String = ""
+
+    'Fonts
+    Dim Title_font As Font = New Font("Tahoma", 14)
+    Dim Tree_font As Font = New Font("Tahoma", 11)
+    Dim Colore_tree As Color = Color.Blue
+    Dim Colore_sfondo As Color = Color.White
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Inserisci un panel con Autoscroll=True consentirà
+        'alla form di generare in automatico le scrollbars.
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'sfoglia
+        FolderBrowserDialog1.SelectedPath = ""
+        FolderBrowserDialog1.Description = "Scegli una cartella..."
+
+        If FolderBrowserDialog1.ShowDialog(Me) = DialogResult.OK Then
+            Tree_path = FolderBrowserDialog1.SelectedPath
+            Dim treebitmap As Bitmap = TreeGrafico(Tree_path)
+            PictureBox1.Image = treebitmap
+            PictureBox1.Width = treebitmap.Width
+            PictureBox1.Height = treebitmap.Height
+            PictureBox1.Refresh()
+        End If
+    End Sub
+
+    Private Function TreeGrafico(path As String) As Bitmap
+        Dim treebitmap = New Bitmap(850, 450)
+        Dim treebitmap_width As Integer = treebitmap.Width
+        Dim treebitmap_height As Integer = treebitmap.Height
+
+        Dim x As Integer = 0
+        Dim y As Integer = 0
+
+        Dim lines As Integer = 0
+        Dim ampiezza As New SizeF
+        Dim Textdraw As String = path & vbCrLf
+
+        Using g As Graphics = Graphics.FromImage(treebitmap)
+            g.Clear(Colore_sfondo)
+            Dim p As New Pen(Colore_tree)
+            Dim b As New SolidBrush(Colore_tree)
+
+            'Cartelle
+            Dim folders() As String = IO.Directory.GetDirectories(path)
+            For Each folder As String In folders
+                ' Do work, example
+                Try
+                    Textdraw = Textdraw & "  |--- " & System.IO.Path.GetFileName(folder) & vbCrLf
+                Catch ex As Exception
+                    MsgBox("Errore: " & ex.Message)
+                End Try
+            Next
+
+            'Salva il testo
+            'My.Computer.FileSystem.WriteAllText(Application.StartupPath & "\temp.txt", Textdraw, False)
+            'System.Threading.Thread.Sleep(200)
+
+            RichTextBox1.Text = Textdraw
+
+            'Misua linee e ampiezza testo
+            lines = RichTextBox1.Lines.Count
+            ampiezza = g.MeasureString(Textdraw, Tree_font)
+
+        End Using
+
+        'Final Bitmap
+        Dim newwidth As Integer = ampiezza.Width + 200 ' + Testo TreeGrafico
+        Dim newheight As Integer = ampiezza.Height + 60 ' + Testo TreeGrafico
+
+        Dim treebitmap_new = New Bitmap(newwidth, newheight)
+        Dim treebitmap_newwidth As Integer = treebitmap_new.Width
+        Dim treebitmap_newheight As Integer = treebitmap_new.Height
+
+        Using g As Graphics = Graphics.FromImage(treebitmap_new)
+            g.Clear(Colore_sfondo)
+            Dim p As New Pen(Colore_tree)
+            Dim b As New SolidBrush(Colore_tree)
+            'g.DrawRectangle(p, 3, 3, treebitmap_width - 6, treebitmap_height - 6)
+            g.DrawString("TreeGrafico " & DateAndTime.Now.ToString & vbCrLf & vbCrLf, Title_font, b, 13, 13)
+            g.DrawString(Textdraw, Tree_font, b, 40, 40)
+        End Using
+
+        'Files al momento non richiesti
+
+        'Dim files() As String = IO.Directory.GetFiles(path)
+        'For Each file As String In files
+        ' Do work, example
+        'Try
+        'Dim text As String = IO.File.ReadAllText(file)
+        'Catch ex As Exception
+        'MsgBox("Errore: " & ex.Message)
+        'End Try
+
+        'Next
+
+        Return treebitmap_new
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Font
+
+        FontDialog1.Font = New Font("Tahoma", 14)
+        If FontDialog1.ShowDialog(Me) = DialogResult.OK Then
+            If FontDialog1.Font IsNot Nothing Then
+                Title_font = FontDialog1.Font
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Font tree
+        FontDialog1.Font = New Font("Tahoma", 11)
+        If FontDialog1.ShowDialog(Me) = DialogResult.OK Then
+            If FontDialog1.Font IsNot Nothing Then
+                Tree_font = FontDialog1.Font
+            End If
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Colore..
+        If ColorDialog1.ShowDialog(Me) = DialogResult.OK Then
+            If ColorDialog1.Color <> Nothing Then
+                Colore_tree = ColorDialog1.Color
+                Label4.BackColor = ColorDialog1.Color
+            End If
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Sfondo
+        If ColorDialog1.ShowDialog(Me) = DialogResult.OK Then
+            If ColorDialog1.Color <> Nothing Then
+                Colore_sfondo = ColorDialog1.Color
+                Label5.BackColor = ColorDialog1.Color
+            End If
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Salva bmp
+        SaveFileDialog1.Title = "Salva il TreeGrafico..."
+        SaveFileDialog1.FileName = "TreeGrafico.png"
+        SaveFileDialog1.Filter = "Files Portable Network Graphics|*.png|Files Bitmap|*.bmp|Files JPEG|*.jpg"
+
+        If SaveFileDialog1.ShowDialog(Me) = DialogResult.OK Then
+            If SaveFileDialog1.FileName <> "" Then
+                Select Case SaveFileDialog1.FileName.Split(".").Last.ToLower
+                    Case "png"
+                        PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Png)
+                    Case "jpg"
+                        PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Jpeg)
+                    Case "bmp"
+                        PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Bmp)
+                End Select
+                MsgBox("TreeGrafico salvato")
+
+            End If
+        End If
+    End Sub
+End Class
